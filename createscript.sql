d4e2d8ae84ee816e57845868333ca480ee573e99
Omid <153293580+Omid2831@users.noreply.github.com>
2024-11-13 15:52:21 +0100

    creating info

--- createscript.sql
+++ createscript.sql
@@ -1,4 +1,4 @@
--- Step one 
+-- Step 1 
 -- ***********************
 -- Aim : creating a Datebaase on this folder 
 -- ***********************
@@ -15,7 +15,7 @@ create database `rollercoaster-2408c`;
 -- Use Datebase rollercoaster-2408c
 use `rollercoaster-2408c`;
 
--- Step two 
+-- Step 2 
 -- ***********************
 -- Aim : creating a Table on this folder 
 -- ***********************
@@ -24,7 +24,7 @@ use `rollercoaster-2408c`;
 -- 1.0.0.0              13-11-2024          OmidMhr         Achtbaan of EU 
 -- ***********************
 create table RollercoasterOfEu (
-    Id smallint unsigned not null auto_increment,
+    Id smallint unsigned not null AUTO_INCREMENT,
     Name_ID varchar(50) not null,
     park varchar(50) not null,
     country varchar(50) not null,
@@ -36,3 +36,36 @@ create table RollercoasterOfEu (
     UpdatedDate datetime(6) not null,
     constraint PK_RollercoassterOfEu_Id primary key clustered(Id)
 ) ENGINE = InnoDB;
+
+-- Step 3 
+-- ***********************
+-- Aim : Fill in the Table with some data
+-- ***********************
+-- Version             Date                Authour          Description
+-- *******             ****                *******          ***********
+-- 1.0.0.0              13-11-2024          OmidMhr         rollercoaster of EU 
+-- ***********************
+insert into
+    RollercoasterOfEu (
+        Name_ID,
+        park,
+        country,
+        topspeed,
+        height,
+        IsActive,
+        Remark,
+        CreatedDate,
+        UpdatedDate,
+    )
+values
+    (
+        "red Force",
+        "Ferrari Land",
+        "Spain",
+        180,
+        112,
+        1, 
+        null,
+        sysdate(6),
+        sysdate(6)
+    );
