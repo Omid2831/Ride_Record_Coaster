6993f6dfe49e149b27a7d5eff30b219b56d75f26
Omid <153293580+Omid2831@users.noreply.github.com>
2024-12-11 14:35:00 +0100

    adding the notification data has been deleted

--- delete.php
+++ delete.php
@@ -26,10 +26,51 @@ where id = :Id";
 /* method prepare in de PDO */
 $statement = $pdo->prepare($sql);
 
-$statement -> bindValue(':Id' , $_GET['Id'], PDO::PARAM_INT);
+$statement->bindValue(':Id', $_GET['Id'], PDO::PARAM_INT);
 
 /* EXECUTE THE CODE */
 
 $statement->execute();
+/* give the user a notification so they can see what is going on! */
 
+
+header('Refresh:2, url=index.php');
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-8">
+                <div class="alert alert-success text-center" role="alert">
+                    Data has been deleted!
+                </div>
+            </div>
+            <div class="col-2"></div>
+        </div>
+    </div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
